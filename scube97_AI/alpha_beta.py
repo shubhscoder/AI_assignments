6c4c23a56f5a0ef7256f80ed69b7fbf3eef53cb1
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2018-10-25 12:25:05 +0530

    Added UI for alpha beta

--- scube97_AI/alpha_beta.py
+++ scube97_AI/alpha_beta.py
@@ -8,6 +8,8 @@ Use python3
 '''
 import random
 import queue
+from turtle import *
+s = 8
 class node:
 	def __init__(self,val,child):
 		self.val = val
@@ -52,6 +54,25 @@ def raw_alpha_beta(dept,motive,alpha,beta):
 		p.val = best
 		return (best,p)
 
+def dfs(root):
+	my_child_and_me = []
+	my_child_and_me.append(str(root.val))
+	child_only = []
+	for i in root.child_ptr:
+		if i!=None:
+			temp_list = dfs(i)
+			if isinstance(temp_list,list):
+				for i in temp_list:
+					child_only.append(i)
+			else:
+				child_only.append(temp_list)
+	if len(child_only)>0:
+		my_child_and_me.append(child_only)
+	if len(my_child_and_me)==1: #leaf node
+		return my_child_and_me[0]
+	return my_child_and_me #any other node
+
+
 def bfs(root):
 	q = queue.Queue()
 	q.put(root)
@@ -62,8 +83,38 @@ def bfs(root):
 			if i!=None:
 				q.put(i)
 
+def cntstrs(list):
+    return len([item for item in list if isinstance(item, str)])
+
+def drawtree(tree, pos,dept,head=0):
+    c = cntstrs(tree)
+    while len(tree):
+        goto(pos)
+        item = tree.pop(0)
+        if head:
+            write(item,1)
+            drawtree(tree.pop(0),pos,dept+1)
+        else:
+            if isinstance(item, str):
+                newpos = ((pos[0] + s*c/4 - s*cntstrs(tree))*3.4, pos[1] - 15*s)
+                down()
+                goto((newpos[0], newpos[1] + 15))
+                up()
+                goto(newpos)
+                write(item,1)
+            elif isinstance(item, list):
+                drawtree(item,newpos,dept+1)
+
+def display_tree(string_arr):
+	myWin = Screen()
+	startpos = (0,200)
+	up()
+	drawtree(string_arr, startpos,0,1)
+	myWin.exitonclick()
+
 root = raw_alpha_beta(0,1,-MAX,MAX)[1]
 bfs(root)
+display_tree(dfs(root))
 
 '''
 No input needed
